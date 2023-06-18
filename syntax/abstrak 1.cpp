7e63226c3e7ccf10efa16956209dc6f470f5222f
PFebrianoooo <putrapebriano27@gmail.com>
2023-06-18 23:40:38 +0700

    first commit

---
+++ syntax/abstrak 1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main (){
+	std::cout << "hallo bandung" <<" kamu anak baik" << std::endl;
+	std::cout << "ibu kota parahyangan";
+	std::cout << " bandung anak yang baik";
+	std::cin.get();
+	return 0;
+}
